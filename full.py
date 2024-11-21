24048517585b4bcd098a31291bd33edf88ee96c3
oosmedy <oosmedyy@gmail.com>
2024-11-22 03:17:47 +0330

    Create a.py

---
+++ full.py
@@ -0,0 +1,27 @@
+import telebot
+import mysql.connector
+print('Initializing Service...')
+bot=telebot.TeleBot('7118472107:AAH12P5cqTcHhySMgGhWEV2grvESLWuLP40')
+connection = mysql.connector.connect( 
+    password='2Kv3SdxgkY',
+    user='sql12745700',
+    host='52.76.27.242',
+    database='sql12745700',
+    )
+mycursor=connection.cursor()
+
+def datalogger(message):
+    sql='INSERT INTO telegrambot_tbl (`name`,`username`,`message`) VALUES(%s,%s,%s)'
+    val=(message.chat.first_name,message.chat.id,message.text)
+    mycursor.execute(sql,val)
+    connection.commit()
+@bot.message_handler()
+def welcome(message):
+    bot.reply_to(message,'recived messsage✅')
+    #datalogger(message)
+    mess="🔴From :"+message.chat.first_name+"\n message :⬇️⬇️⬇️ \n"+message.text
+    bot.send_message(6269684719,mess)
+    print('New Message From ',message.chat.first_name,"ID:",message.chat.id)
+    
+print('Normal Status')    
+bot.polling()    
