40b537294d1a8eda70a8ac572929bd2916a0b494
oosmedy <oosmedyy@gmail.com>
2024-11-28 13:56:28 +0330

    Update full.py
    هموجو

--- full.py
+++ full.py
@@ -1,28 +1,30 @@
-import telebot
-import mysql.connector
-print('Initializing Service...')
-bot=telebot.TeleBot('TELEGRAM API TOKEN FROM BOT FATHER')
-connection = mysql.connector.connect( 
-    password='2Kv3SdxgkY',
-    user='sql12745700',
-    host='localhost',
-    database='sql12745700',
-    )
-mycursor=connection.cursor()
+import requests
 
-def datalogger(message):
-    sql='INSERT INTO telegrambot_tbl (`name`,`username`,`message`) VALUES(%s,%s,%s)'
-    val=(message.chat.first_name,message.chat.id,message.text)
-    mycursor.execute(sql,val)
-    connection.commit()
-@bot.message_handler()
-def welcome(message):
-    bot.reply_to(message,'recived messsage✅')
-    #datalogger(message)
-    mess="🔴From :"+message.chat.first_name+"\n message :⬇️⬇️⬇️ \n"+message.text
-    ownerid=0000000
-    bot.send_message(ownerid,mess)
-    print('New Message From ',message.chat.first_name,"ID:",message.chat.id)
-    
-print('Normal Status')    
-bot.polling()    
+url = "https://sedayezende.ir/"
+cookies = {'PHPSESSID': '9olesjraphcramjc2b11g7elns'}
+headers = {
+    'Host': 'sedayezende.ir',
+    'Content-Length': '53',
+    'Cache-Control': 'max-age=0',
+    'Sec-Ch-Ua': '"Chromium";v="109", "Not_A Brand";v="99", "Google Chrome";v="109"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Upgrade-Insecure-Requests': '1',
+    'Origin': 'https://sedayezende.ir',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Referer': 'https://sedayezende.ir/',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9'
+}
+data = {'domain': 'Digikala.com', 'submitAddNewScore': '1', 'score': '20'}
+l=0
+while True:
+ response = requests.post(url, headers=headers, cookies=cookies, data=data)
+ print(l)
+ l+=1
